2a2dc157bc2646aae88d155e43e058935e9b7793
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-04-10 22:02:47 +0530

    lib folder and prisma connection

---
+++ lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client/extension';
+
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter: pool });
+
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
